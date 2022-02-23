c6ed9815af73f3f52392effac2b781206a9c0dac
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-02-23 09:00:18 +0000

    [Add]商品編集画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,17 +1,18 @@
 class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
+    @genres = Genre.all
   end
 
   def create
     item = Item.new(item_params)
     item.save
-    redirect_to admin_items_path
+    redirect_to admin_item_path(item.id)
   end
 
-
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page])
+    @genres = Genre.all
   end
 
   def show
@@ -20,13 +21,20 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path
   end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price)
+    params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'dc0d5080787b4971d0140367ac0285efea09b21c0720c545ed44559f1cf7cbd9fca298679da9cc516047deaf86cd0e349e6f4c59eeaf7a3515484b84c3962920'
